41c901e05f49a7a9fc6c71d063bd9ab82824d9cf
tushar thakur <tusharthakur977@gmail.com>
2022-07-05 12:47:13 +0530

    frontend navbar and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,27 @@
-import './App.css';
+import React, { useState, useEffect } from "react";
+
+import { BrowserRouter } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Layout/header/Header.js";
+import WebFont from "webfontloader";
+import Footer from "./components/Layout/footer/Footer";
 
 function App() {
+  React.useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ["Roboto", "Droid Sans", "Chilanka"],
+      },
+    });
+  }, []);
+
   return (
     <div className="App">
-  
+      <BrowserRouter>
+        <Header />
+
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
